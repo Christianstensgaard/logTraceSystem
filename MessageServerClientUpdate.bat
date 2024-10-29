4f97108d70e619aaa86966b9a20c0e1e294d307e
Christian <christian.lsj@outlook.dk>
2024-10-29 17:02:40 +0100

    Updated MessageServerClient Git

--- MessageServerClientUpdate.bat
+++ MessageServerClientUpdate.bat
@@ -1,6 +1,4 @@
 @echo off
-:: Handling Updating the Message Server and Client from external Git
-
 set REPO_URL=https://github.com/Christianstensgaard/MessageServerClient.git
 set SUBTREE_PREFIX=messageServerClient
 set TARGET_BRANCH=master
@@ -13,11 +11,23 @@ if %ERRORLEVEL% NEQ 0 (
 
 cd /d %~dp0
 
-echo Updating the messageServerClient repository...
-git subtree pull --prefix=%SUBTREE_PREFIX% %REPO_URL% %TARGET_BRANCH% --squash
-
-if %ERRORLEVEL% EQU 0 (
-    echo "messageServerClient has been successfully updated."
+echo Checking if messageServerClient subtree exists...
+git ls-tree -d HEAD %SUBTREE_PREFIX% >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo "messageServerClient subtree not found. Adding subtree..."
+    git subtree add --prefix=%SUBTREE_PREFIX% %REPO_URL% %TARGET_BRANCH% --squash
+    if %ERRORLEVEL% EQU 0 (
+        echo "messageServerClient subtree has been successfully added."
+    ) else (
+        echo "Failed to add messageServerClient. Please check the repository URL and try again."
+        exit /b 1
+    )
 ) else (
-    echo "Failed to update messageServerClient. Please check the repository URL and try again."
+    echo "messageServerClient subtree found. Updating..."
+    git subtree pull --prefix=%SUBTREE_PREFIX% %REPO_URL% %TARGET_BRANCH% --squash
+    if %ERRORLEVEL% EQU 0 (
+        echo "messageServerClient has been successfully updated."
+    ) else (
+        echo "Failed to update messageServerClient. Please check the repository URL and try again."
+    )
 )
--- TraceService/obj/Debug/net8.0/TraceService.AssemblyInfo.cs
+++ TraceService/obj/Debug/net8.0/TraceService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraceService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed763608b0bcf80e1f8a3a349fcddf5930186549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef7ab5c210ee193cdaac5122ff1c5ca1b20e4db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraceService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraceService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
