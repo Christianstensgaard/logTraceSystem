53a4e7d5e3d5115e93695103afe928f2840cd79d
Christian <christian.lsj@outlook.dk>
2024-10-29 10:11:06 +0100

    Squashed 'messageServerClient/' content from commit 062d100
    git-subtree-dir: messageServerClient
    git-subtree-split: 062d100beca3fb350b1b8fedb8c7512965a1f995

---
+++ messageServer/ServiceController.cs
@@ -0,0 +1,29 @@
+using System.Net.Sockets;
+using BitToolbox;
+
+public class ServiceController{
+  public ServiceController(){
+    bufferController = new ByteBufferController();
+    connections = new List<SlaveConnection>();
+  }
+  
+  public void NewConnection(TcpClient socket){
+      connections.Add(new SlaveConnection(){
+        Socket = socket
+      });
+  }
+
+
+  
+
+
+
+
+
+    class SlaveConnection{
+    public byte[] Header {get;set;}
+    public TcpClient? Socket {get; set; }
+  }
+    readonly List<SlaveConnection> connections;
+    readonly ByteBufferController bufferController;
+}
