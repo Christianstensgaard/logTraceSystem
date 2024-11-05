316e677d41fe37080f2d91252365b99a89c8eb49
Christian <christian.lsj@outlook.dk>
2024-11-05 01:23:49 +0100

    Squashed 'MessageServerClient/' content from commit eb599ee
    git-subtree-dir: MessageServerClient
    git-subtree-split: eb599ee795735074e3c2c64db9890d79a630468b

---
+++ lib/MsPipeline/Pipeline.cs
@@ -0,0 +1,10 @@
+﻿namespace MsPipeline;
+
+public class Pipeline
+{
+
+
+
+
+
+}
---
+++ messageServer/Program.cs
@@ -0,0 +1,14 @@
+﻿//- Message Server
+
+using System.Net.Sockets;
+using BitToolbox;
+using MessageServer;
+
+
+NetworkController network = new NetworkController();
+network.Start();
+bool Running = true;
+while(Running){
+  Thread.Sleep(2000);
+  //- If something else is needed in the future!
+}
