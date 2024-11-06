5c0a1b14b04ac40cd32d03c966f6cf8a641f316d
CHJOR <christian.lsj@outlook.dk>
2024-11-06 19:27:02 +0100

    Added Demo inside TraceService

--- TraceService/Program.cs
+++ TraceService/Program.cs
@@ -16,6 +16,8 @@ public class UpdateTrace : TaskService
 {
     public UpdateTrace(string taskName) : base("Update Task")
     {
+      ErrorDefinitions.TTD = 10; //Time to die
+      ErrorDefinitions.CallOnConnectionLost = true; // call error if connection is lost
     }
 
 
@@ -29,12 +31,12 @@ public class UpdateTrace : TaskService
        Trace trace = new Trace();
        UnPackStream(trace);
 
+
+
       //- ServiceEngine.I Will be removed, And wrapped inside the TaskService class
-      
       if(trace.ID == 0)
         ServiceEngine.I.CallBackRequest(0xf4, "Logger", "Error", [0xff], ()=>{
             //- Response from the service
-
             return true;
         });
       else ServiceEngine.I.ReturnDone();
