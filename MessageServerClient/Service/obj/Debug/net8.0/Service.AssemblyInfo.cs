4ee83cfb3f30e948559accc8c0ecd820a0366cee
CHJOR <christian.lsj@outlook.dk>
2024-11-06 19:25:11 +0100

    Added Demo inside TraceService

--- MessageServerClient/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ MessageServerClient/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a5446a8f6e2ba9d0eee5347777230daefb748d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8feae48fde53a05164fd9bd54619bcbd5dab8aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TraceService/Program.cs
+++ TraceService/Program.cs
@@ -1,9 +1,72 @@
-﻿using Service;
+﻿using MsPipeline;
+using Service;
+using Service.Core;
 using TraceService.Services;
 
-ToolBox.RunTime.ServiceClientName = "traceService";
-ToolBox.RunTime.Start("", 20200);
+TaskBuilder.CreateTask("CreateTrace", new Trace(), ()=>{
 
+
+
+
+  return true;
+} );
+
+
+public class UpdateTrace : TaskService
+{
+    public UpdateTrace(string taskName) : base("Update Task")
+    {
+    }
+
+
+    public override void OnError(ErrorModel error)
+    {
+        throw new Exception(error.Message);
+    }
+
+    public override void OnInvoke(byte[] payload)
+    {
+       Trace trace = new Trace();
+       UnPackStream(trace);
+
+      //- ServiceEngine.I Will be removed, And wrapped inside the TaskService class
+      
+      if(trace.ID == 0)
+        ServiceEngine.I.CallBackRequest(0xf4, "Logger", "UpdateError", [0xff], ()=>{
+            //- Response from the service
+
+            return true;
+        });
+      else ServiceEngine.I.ReturnDone();
+    }
+}
+
+
+
+
+
+
+
+
+
+public class Trace : ISerilize
+{
+    public bool IsValid { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public int ID { get; set; }
+
+
+    public byte Pack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Unpack(byte[] stream)
+    {
+        throw new NotImplementedException();
+    }
+
+}
 
 
 
